fb23313170199f5b67d9cc8eefa060bd7916a9dc
Szupera Zita <szuperaz@gmail.com>
2015-02-08 15:38:16 +0100

    Question4 solved

--- src/questions/chapter1/Question4.java
+++ src/questions/chapter1/Question4.java
@@ -1,5 +1,7 @@
 package questions.chapter1;
 
+import java.util.Arrays;
+
 /**
  * Created by szuperaz on 08/02/15.
  */
@@ -7,6 +9,12 @@ package questions.chapter1;
 public class Question4 {
 
     public boolean anagrams (String string1, String string2) {
-        return false;
+        string1 = string1.replace(" ", "");
+        string2 = string2.replace(" ", "");
+        char[] string1Array = string1.toCharArray();
+        char[] string2Array = string2.toCharArray();
+        Arrays.sort(string1Array);
+        Arrays.sort(string2Array);
+        return Arrays.equals(string1Array, string2Array);
     }
 }
